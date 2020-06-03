f97c28d574191bc2aa0fb7fa07b0a082dcd14c4d
Henry Le <henryle10@gmail.com>
2020-06-02 17:47:51 -0700

    added side navbar

--- client/src/views/AllProducts.js
+++ client/src/views/AllProducts.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, navigate } from "@reach/router";
 import styles from '../styles/TechStyle.module.css';
+import SideNavBar from '../components/SideNavBar';
 
 const Products = (props) => {
     const [products, setProducts] = useState(null);
@@ -36,17 +37,19 @@ const Products = (props) => {
 
 
     return (
-        <div className="text-left p-5">
-            <h2 className={styles.textTech}>View All Tech Products</h2>
-            <h2>fuck you peppy</h2>
-            <div className="d-flex flex-wrap mt-5">
-                {products.map((product) => {
-                    return (
-                        <div className={`m-3 ${styles.techProductDiv}`} key={product._id}>
-                            <Link to={`/product/${product._id}`}><img className={`img-fluid ${styles.techImg}`} src={product.imgUrl} alt={product.title} /></Link>
-                            <p className="font-weight-bold m-0">{product.title}</p>
-                            <p>Price: ${product.price}</p>
-                            {/* <button
+        <div className="text-left row">
+            <SideNavBar></SideNavBar>
+            <div className="col-md-10">
+                <h2 className={styles.textTech}>View All Tech Products</h2>
+                <h2>fuck you peppy</h2>
+                <div className="d-flex flex-wrap mt-5">
+                    {products.map((product) => {
+                        return (
+                            <div className={`m-3 ${styles.techProductDiv}`} key={product._id}>
+                                <Link to={`/product/${product._id}`}><img className={`img-fluid ${styles.techImg}`} src={product.imgUrl} alt={product.title} /></Link>
+                                <p className="font-weight-bold m-0">{product.title}</p>
+                                <p>Price: ${product.price}</p>
+                                {/* <button
                                 onClick={(event) => { handleDelete(product._id); }}
                                 className="btn btn-danger">
                                 Delete
@@ -56,9 +59,10 @@ const Products = (props) => {
                                 className="btn btn-warning">
                                 Edit
                             </button> */}
-                        </div>
-                    );
-                })}
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
